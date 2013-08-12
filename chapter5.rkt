0c82bd6047c1bb685659e7e8b0a2ea090920e1d8
Jingjing Duan <jingjing.duan@hulu.com>
2013-08-12 10:07:48 -0700

    Exercise 5.1.3

--- chapter5.rkt
+++ chapter5.rkt
@@ -23,3 +23,15 @@
 (symbol=? 'Perfect (check-guess 1 1))
 (symbol=? 'TooSmall (check-guess 1 2))
 (symbol=? 'TooLarge (check-guess 1 0))
+
+;; Exercise 5.1.3
+
+(define (form-number ones tens hundreds)
+  (+ ones (* 10 tens) (* 100 hundreds)))
+
+(define (check-guess3 ones tens hundreds target)
+  (check-guess (form-number ones tens hundreds) target))
+
+(symbol=? 'TooSmall (check-guess3 1 2 3 500))
+(symbol=? 'TooLarge (check-guess3 1 2 3 100))
+(symbol=? 'Perfect (check-guess3 1 2 3 321))
