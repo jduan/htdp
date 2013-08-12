50e976876c90ca5850c017a7d28cc016de5b9178
Jingjing Duan <jingjing.duan@hulu.com>
2013-08-11 23:27:51 -0700

    Exercise 4.2.1

--- chapter4.rkt
+++ chapter4.rkt
@@ -15,3 +15,20 @@
 (= (* x x) x) ;; false when x = 4
 (= (* x x) x) ;; false when x = 2
 (= (* x x) x) ;; false when x = 7/2
+
+;; Exercise 4.2.1
+(define (greater-than-3-less-or-equal-to7 x)
+  (and (> x 3) (<= 7)))
+
+(define (between-3-and-7 x)
+  (and (>= x 3) (<= x 7)))
+
+(define (ge-3-l-9 x)
+  (and (>= x 3) (< x 9)))
+
+(define (union x)
+  (or (and (> x 1) (< x 3))
+      (and (> x 9) (< x 11))))
+
+(define (outside x)
+  (not (and (>= x 1) (<= x 3))))
