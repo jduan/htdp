032e9357a69d1ad7e480f5a246480c8f3b1c80ad
Jingjing Duan <jingjing.duan@hulu.com>
2013-08-11 22:01:23 -0700

    Exercise 3.2.1

--- chapter3.rkt
+++ chapter3.rkt
@@ -44,3 +44,37 @@
 (profit 3)
 (profit 4)
 (profit 5)
+
+;; Exercise 3.1.4
+(define (cost ticket-price)
+  (* 1.5 (attendees ticket-price)))
+
+;; Exercise 3.2.1
+(define BASE-PRICE 5)
+(define PRICE-DROP-UNIT 0.1)
+(define BASE-ATTENDEES 120)
+(define ATTENDEES-BY-PRICE-DROP 15)
+(define BASE-COST 180)
+(define COST-PER-ATTENDEE 0.04)
+
+(define (price n)
+  (- BASE-PRICE (* PRICE-DROP-UNIT n)))
+
+(define (num-of-people n)
+  (+ BASE-ATTENDEES (* ATTENDEES-BY-PRICE-DROP n)))
+
+(define (cost n)
+  (+ BASE-COST (* COST-PER-ATTENDEE (num-of-people n))))
+
+(define (revenue n)
+  (* (price n) (num-of-people n)))
+
+(define (profit n)
+  (- (revenue n) (cost n)))
+
+(profit 0)
+(profit 10)
+(profit 20)
+(profit 30)
+(profit 40)
+(profit 50)
