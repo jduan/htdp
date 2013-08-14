b81f4aa5fcb2595d1a24c19696ba43192eb99a7c
Jingjing Duan <jingjing.duan@hulu.com>
2013-08-13 23:40:16 -0700

    Exercise 6.1.1 and 6.2.2

---
+++ chapter6.rkt
@@ -0,0 +1,49 @@
+(require lang/posn)
+;; Exercise 6.1.1
+
+(define (distance-to-0 a-posn)
+  (sqrt
+    (+ (sqr (posn-x a-posn))
+       (sqr (posn-y a-posn)))))
+
+(= 5 (distance-to-0 (make-posn 3 4)))
+(= 10 (distance-to-0 (make-posn (* 2 3) (* 2 4))))
+(= 13 (distance-to-0 (make-posn 12 (- 6 1))))
+
+;; Exercise 6.2.2
+(define WIDTH 50)
+(define HEIGHT 160)
+(define BULB-RADIUS 20)
+(define BULB-DISTANCE 10)
+
+;; the positions of the bulbs 
+(define X-BULBS (quotient WIDTH 2))
+(define Y-RED (+ BULB-DISTANCE BULB-RADIUS))
+(define Y-YELLOW (+ Y-RED BULB-DISTANCE (* 2 BULB-RADIUS)))
+(define Y-GREEN (+ Y-YELLOW BULB-DISTANCE (* 2 BULB-RADIUS)))
+
+;; draw the light with the red bulb turned on
+(start WIDTH HEIGHT)
+(draw-solid-disk (make-posn X-BULBS Y-RED) BULB-RADIUS 'red)
+(draw-circle (make-posn X-BULBS Y-YELLOW) BULB-RADIUS 'yellow)
+(draw-circle (make-posn X-BULBS Y-GREEN) BULB-RADIUS 'green)
+
+(define (draw-bulb color)
+  (cond
+    [(symbol=? color 'red) (draw-solid-disk (make-posn X-BULBS Y-RED) BULB-RADIUS 'red) ]
+    [(symbol=? color 'yellow) (draw-solid-disk (make-posn X-BULBS Y-YELLOW) BULB-RADIUS 'yellow) ]
+    [(symbol=? color 'green) (draw-solid-disk (make-posn X-BULBS Y-GREEN) BULB-RADIUS 'green) ]
+    [else "wrong color"]))
+
+(define (clear-bulb color)
+  (cond
+    [(symbol=? color 'red)
+     (and (clear-solid-disk (make-posn X-BULBS Y-RED) BULB-RADIUS 'red)
+          (draw-circle (make-posn X-BULBS Y-RED) BULB-RADIUS 'red))]
+    [(symbol=? color 'yellow)
+     (and (clear-solid-disk (make-posn X-BULBS Y-YELLOW) BULB-RADIUS 'yellow)
+          (draw-circle (make-posn X-BULBS Y-YELLOW) BULB-RADIUS 'yellow))]
+    [(symbol=? color 'green)
+     (and (clear-solid-disk (make-posn X-BULBS Y-GREEN) BULB-RADIUS 'green)
+          (draw-circle (make-posn X-BULBS Y-GREEN) BULB-RADIUS 'green))]
+    [else "wrong color"]))
