8d846ea4f30d54d2dc2324b2ea550fda04cbb7a5
Jingjing Duan <jingjing.duan@hulu.com>
2013-08-17 13:01:39 -0700

    Exercise 9.3.1

--- chapter8.rkt
+++ chapter8.rkt
@@ -60,3 +60,16 @@
       (symbol=? 'doll (first (rest lst)))))
 (contains-2-doll? '(doll what))
 (contains-2-doll? '(hello what))
+
+;; Exercise 9.3.1
+(define (contains-doll? a-list-of-symbols)
+  (cond
+    [(empty? a-list-of-symbols) false]
+    [else (cond
+            [(symbol=? 'doll (first a-list-of-symbols)) true]
+            [else (contains-doll? (rest a-list-of-symbols))])]))
+
+(contains-doll? empty)
+(contains-doll? '(ball))
+(contains-doll? '(arrow doll))
+(contains-doll? '(bow arrow ball))
