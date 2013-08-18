1c126bc3ac3983b02f64506f22657533068f2d8b
Jingjing Duan <jingjing.duan@hulu.com>
2013-08-18 10:10:19 -0700

    Exercise 11.2.2

--- chapter11.rkt
+++ chapter11.rkt
@@ -12,3 +12,20 @@
     empty
     (cons sym (repeat (sub1 n) sym))))
 (equal? '(haha haha haha) (repeat 3 'haha))
+
+;; Exercise 11.2.2
+(define (f x)
+  (+ (* 3 (* x x))
+     (+ (* -6 x)
+        -1)))
+(define (tabulate-f n)
+  (if (zero? n)
+    empty
+    (cons (make-posn n (f n))
+          (tabulate-f (sub1 n)))))
+(equal? (cons (make-posn 4 23)
+              (cons (make-posn 3 8)
+                    (cons (make-posn 2 -1)
+                          (cons (make-posn 1 -4)
+                                empty))))
+        (tabulate-f 4))
