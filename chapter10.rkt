8769e90e2a35db181faad4d8133265265b966d77
Jingjing Duan <jingjing.duan@hulu.com>
2013-08-17 22:22:39 -0700

    Exercise 10.1.2

--- chapter10.rkt
+++ chapter10.rkt
@@ -11,3 +11,19 @@
 (equal? '(140 280 420) (hours->wages '(10 20 30)))
 (equal? '(28 196 2954) (hours->wages '(2 14 211)))
 
+;; Exercise 10.1.2
+;; calculate the total wage of someone who worked for h hours
+(define (wage h)
+  (if (> h 100)
+    (error 'wage "too many hours")
+    (* 14 h)))
+
+;; calculate the wages for a given list of hours
+(define (hours->wages a-list-of-hours)
+  (cond
+    [(empty? a-list-of-hours) empty]
+    [else (cons (wage (first a-list-of-hours))
+                (hours->wages (rest a-list-of-hours)))]))
+(equal? '(140 280 420) (hours->wages '(10 20 30)))
+(equal? '(28 196 2954) (hours->wages '(2 14 211)))
+
