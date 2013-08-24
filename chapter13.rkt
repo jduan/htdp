aa8cbd923e1d42ce7654c1abc09ba0b5d1693c15
Jingjing Duan <jingjing.duan@hulu.com>
2013-08-23 22:08:13 -0700

    Exercise 13.0.4

--- chapter13.rkt
+++ chapter13.rkt
@@ -17,3 +17,13 @@
               (cons (cons 1 (cons 2 empty))
                     (cons (cons 1 (cons 2 (cons 3 empty)))
                           empty))))
+
+;; Exercise 13.0.4
+(equal? (cons 'a (cons 'b (cons 'c (cons 'd (cons 'e empty)))))
+        (list 'a 'b 'c 'd 'e))
+(equal? (cons (cons 1 (cons 2 empty)) empty)
+        (list (list 1 2)))
+(equal? (cons 'a (cons (cons 1 empty) (cons false empty)))
+        (list 'a (list 1) false))
+(equal? (cons (cons 1 (cons 2 empty)) (cons (cons 2 (cons 3 empty)) empty))
+        (list (list 1 2) (list 2 3)))
