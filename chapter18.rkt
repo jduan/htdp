4829bc0a36a8dc6a8270247ae8e522991aeed8c5
Jingjing Duan <jingjing.duan@hulu.com>
2013-09-13 23:30:32 -0700

    Exercise 18.1.1

---
+++ chapter18.rkt
@@ -0,0 +1,18 @@
+;; Exercise 18.1.1
+(local [(define (f x) (+ x 5))
+        (define (g alon)
+          (cond
+            [(empty? alon) empty]
+            [else (cons (f (first alon)) (g (rest alon)))]))]
+       (g (list 1 2 3)))
+
+(local [(define (odd an)
+          (cond
+            [(zero? an) false]
+            [else (even (sub1 an))]))
+        (define (even an)
+          (cond
+            [(zero? an) true]
+            [else (odd (sub1 an))]))]
+       (even 12))
+
