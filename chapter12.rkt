537943abf6383d7348aabb5a381257eb8a034a30
Jingjing Duan <jingjing.duan@hulu.com>
2013-09-14 14:02:48 -0700

    Exercise 18.1.10

--- chapter12.rkt
+++ chapter12.rkt
@@ -113,3 +113,35 @@
 (arrangements '(a))
 (arrangements '(a b))
 (arrangements '(a b c))
+
+;; Exercise 18.1.10
+(define-struct parent (children name date eyes))
+
+(define Gustav (make-parent empty 'Gustav 1988 'brown))
+(define Eva (make-parent (list Gustav) 'Eva 1965 'blue))
+(define Fred (make-parent (list Gustav) 'Fred 1966 'pink))
+(define Adam (make-parent empty 'Adam 1950 'yellow))
+(define Dave (make-parent empty 'Dave 1955 'black))
+(define Carl (make-parent (list Adam Dave Eva) 'Carl 1926 'green))
+(define Bettina (make-parent (list Adam Dave Eva) 'Bettina 1926 'green))
+
+(define (blue-eyed-descendant? parent)
+  (local [(define (blue-eyed-descendant? parent)
+            (or
+              (symbol=? 'blue (parent-eyes parent))
+              (blue-eyed-children? (parent-children parent))))
+            (define (blue-eyed-children? children)
+              (cond
+                [(empty? children) false]
+                [else (or (blue-eyed-descendant? (first children))
+                          (blue-eyed-children? (rest children)))]))]
+         (blue-eyed-descendant? parent)))
+
+(equal? false (blue-eyed-descendant? Gustav))
+(equal? false (blue-eyed-descendant? Fred))
+(equal? true (blue-eyed-descendant? Eva))
+(equal? false (blue-eyed-descendant? Dave))
+(equal? false (blue-eyed-descendant? Adam))
+(equal? true (blue-eyed-descendant? Bettina))
+(equal? true (blue-eyed-descendant? Carl))
+
