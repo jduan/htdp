bdb4739ca3951e835ac5627bb21b1e3eb7967273
Jingjing Duan <jingjing.duan@hulu.com>
2013-09-15 17:44:00 -0700

    Exercise 21.2.1

--- chapter21.rkt
+++ chapter21.rkt
@@ -111,3 +111,32 @@
 (equal? (ormap symbol? (cons 'hello (build-list 10 identity))) true)
 (equal? (foldr + 100 (build-list 10 identity)) 145)
 (equal? (foldl + 100 (build-list 10 identity)) 145)
+
+;; Exercise 21.2.1
+(equal? (list 0 1 2 3) (build-list 4 identity))
+(equal? (list 1 2 3 4) (build-list 4 add1))
+(equal? (list .1 .01 .001 .0001) (build-list 4 (lambda (x) (expt 0.1 (add1 x)))))
+(define (evens n)
+  (build-list n (lambda (x) (* 2 x))))
+
+(equal? (evens 2) '(0 2))
+(equal? (evens 5) '(0 2 4 6 8))
+;; To tabulate an operation between n and 0 (inclusive) n a list
+(define (tabulate f n)
+  (reverse (build-list (add1 n) f)))
+
+(equal? (tabulate identity 10) '(10 9 8 7 6 5 4 3 2 1 0))
+(equal? (tabulate identity 4)  (list 4 3 2 1 0))
+(equal? (tabulate sin 4)  (list  (sin 4)  (sin 3)  (sin 2)  (sin 1)  (sin 0)))
+
+;; n -> (listof (listof 0 or 1))
+(define (diagonal n)
+  (local ((define (build-row i)
+            (build-list n (lambda (x) (if (= i x) 1 0)))))
+         (build-list n build-row)))
+
+(equal? (diagonal 3)
+        (list
+          (list 1 0 0)
+          (list 0 1 0)
+          (list 0 0 1)))
