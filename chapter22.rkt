8f874d08e4eea2765af4fe76b0dde3633375f763
Jingjing Duan <jingjing.duan@hulu.com>
2013-09-16 23:23:49 -0700

    Exercise 22.2.2

--- chapter22.rkt
+++ chapter22.rkt
@@ -17,3 +17,23 @@
         (make-ir 'audio 7)
         (make-ir 'ce 100)))
 (equal? '(book video audio ce)((mymap ir-name) inventory-records))
+
+;; Exercise 22.2.2
+;; (X Y -> boolean) -> ((listof X) -> (listof X))
+;; Consumes a comparison function and returns a function that takes a list of
+;; items and sort them using the comparison function.
+(define (mysort cmp)
+  (local ([define (mysort alon)
+           (cond
+             [(empty? alon) alon]
+             [else (insert (first alon) (mysort (rest alon)))])]
+          [define (insert an alon)
+           (cond
+             [(empty? alon) (list an)]
+             [else (cond
+                     [(cmp an (first alon)) (cons an alon)]
+                     [else (cons (first alon) (insert an (rest alon)))])])])
+         mysort))
+
+(equal? ((mysort <) '(2 3 1 5 4)) '(1 2 3 4 5))
+(equal? ((mysort >) '(2 3 1 5 4)) '(5 4 3 2 1))
