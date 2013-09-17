e6407c1a5b8fb43cc18aa9c7cebd559aed0b7192
Jingjing Duan <jingjing.duan@hulu.com>
2013-09-16 23:47:05 -0700

    Exercise 22.2.3

--- chapter22.rkt
+++ chapter22.rkt
@@ -37,3 +37,35 @@
 
 (equal? ((mysort <) '(2 3 1 5 4)) '(1 2 3 4 5))
 (equal? ((mysort >) '(2 3 1 5 4)) '(5 4 3 2 1))
+
+;; Exercise 22.2.3
+;; (X Y ... Z -> Z') init -> ((listof X) -> value)
+;; Consumes a combine function and an initial value, and returns a function that
+;; takes a list of items and "fold" them into a final value.
+;; my own implementation of fold right
+(define (myfoldr combine init)
+  (local ([define (myfoldr lst)
+           (cond
+             [(empty? lst) init]
+             [else (combine (first lst) (myfoldr (rest lst)))])])
+         myfoldr))
+
+(equal? ((myfoldr + 0) '(1 2 3 4)) 10)
+(equal? ((myfoldr - 0) '(1 2 3 4)) -2)
+(equal? ((myfoldr * 1) '(1 2 3 4)) 24)
+(equal? ((myfoldr / 1) '(1 2 3 4)) 3/8)
+(equal? ((myfoldr cons '(1 2 3)) '(4 5 6)) '(4 5 6 1 2 3))
+
+;; my own implementation of fold left
+(define (myfoldl combine init)
+  (local ([define (myfoldl lst acc)
+           (cond
+             [(empty? lst) acc]
+             [else (myfoldl (rest lst) (combine (first lst) acc))])]
+          [define (myfoldl2 lst)
+           (myfoldl lst init)])
+          myfoldl2))
+
+(equal? ((myfoldl - 0) '(1 2 3 4)) 2)
+(equal? ((myfoldl / 1) '(1 2 3 4)) 8/3)
+(equal? ((myfoldl cons '(1 2 3)) '(4 5 6)) '(6 5 4 1 2 3))
