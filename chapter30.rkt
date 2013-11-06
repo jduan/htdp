d9fa7adb8d64432bcd90600af54379f5ce74c50c
Jingjing Duan <jingjing.duan@hulu.com>
2013-11-05 23:16:54 -0800

    route-exists? and route-exists2?

--- chapter30.rkt
+++ chapter30.rkt
@@ -21,4 +21,39 @@
   (helper alon 0))
 
 (check-expect (relative-to-absolute2 (list 50 40 70 30 30)) (list 50 90 160 190 220))
+
+(define SimpleG '((A B)
+                  (B C)
+                  (C E)
+                  (D E)
+                  (E B)
+                  (F F)))
+(define (next-node node sg)
+    (second (assoc node sg)))
+
+(define (route-exists? orig dest sg)
+  (cond
+    [(symbol=? orig dest) true]
+    [else (route-exists? (next-node orig sg) dest sg)]))
+
+(check-expect (route-exists? 'A 'E SimpleG) true)
+(check-expect (route-exists? 'A 'C SimpleG) true)
+
+(define (contains sym lst)
+  (member sym lst))
+
+(define (route-exists2? orig dest sg)
+  (define (re-accu? orig accu-seen)
+    (cond
+      [(symbol=? orig dest) true]
+      [(contains orig accu-seen) false]
+      [else (re-accu? (next-node orig sg) (cons orig accu-seen))]))
+  (re-accu? orig empty))
+
+(check-expect (route-exists2? 'A 'E SimpleG) true)
+(check-expect (route-exists2? 'A 'C SimpleG) true)
+(check-expect (route-exists2? 'A 'D SimpleG) false)
+(check-expect (route-exists2? 'B 'D SimpleG) false)
+
+
 (test)
